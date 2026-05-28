3d89badf9171c66d9f19473244ab59459b43a46a
sushmitatimalsina <sushmitatimalsina602@gmail.com>
2026-05-28 23:17:55 +0545

    feat: add core utility files for colors, text styles, snack bar, string utilities, and validators

---
+++ lib/core/colors.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const primary = Color(0xff4B6BFB);
+  static const grey = Color(0xffE5E5E5);
+  static const textGrey = Colors.grey;
+  static const white = Colors.white;
+  static const black = Colors.black;
+}
---
+++ lib/core/text_style.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'colors.dart';
+
+class AppTextStyles {
+
+  static const heading = TextStyle(
+    fontSize: 28,
+    fontWeight: FontWeight.bold,
+    color: AppColors.black,
+  );
+
+  static const buttonText = TextStyle(
+    fontSize: 22,
+    fontWeight: FontWeight.bold,
+    color: AppColors.white,
+  );
+
+  static const normalText = TextStyle(
+    fontSize: 13,
+    color: AppColors.black,
+  );
+}
